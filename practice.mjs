942b48e35d5af7f7176b0eefc7e78b061691d4fe
practie <93399035+practie@users.noreply.github.com>
2021-11-01 21:30:52 +0500

    Add files via upload

---
+++ practice.mjs
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+const port = process.env.PORT || 3000
+
+app.get("/", (req, res) => {
+    res.send("I am a hello world server practice");
+})
+app.get("/profile", (req, res) => {
+    res.send("here is your profile");
+})
+app.get("/about", (req, res) => {
+    res.send("some information about me");
+})
+app.listen(port, () => {
+    console.log(`server is running on port 3000`);
+});
